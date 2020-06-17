8e78ded882e89c3960a79c736702a8cafdf940ec
Max Tarlov <mtarlov@ucsc.edu>
2020-06-17 10:35:53 -0700

    OTsystem class and docstrings

--- OTtools.py
+++ OTtools.py
@@ -5,22 +5,47 @@ def warn(warning):
     #TODO perhapse made this mutable and add interactive capabilities
 
 class OTobject:
+    """Metaclass for expected contents of a Tableaux instance
+    
+    Ensures that contents of a Tableaux instance support string casting
+
+    Attrabutes:
+        value (any): python representation of the object.
+        id (str): string representation of the object.
+    """
+
     def __init__(self, value):
+        """Initializes new OTobject instance
+        
+        Parameter: value (any): representation of the object.
+            Must be castable to str
+        """
         self.id = str(value)
         self.value = value
+    
     def __str__(self):
         return self.id
     #TODO add __repr__
 
 class Candidate(OTobject):
+    """Extends OTobject
+
+    Nothing extra yet.
+    """
     def foo(self):
         pass
 
 class Constraint(OTobject):
+    """Extends OTobject
+
+    Nothing extra yet.
+    """
     def foo(self):
         pass
 
 class Tableaux:
+    """
+    """
     def __init__(self, matrix):
         self.input = matrix[0][0]
         self.violations = [i[1:] for i in matrix[1:]]
@@ -35,8 +60,22 @@ class Tableaux:
         result = [[str(i) for i in j] for j in self._matrixRows]
         return str(result)
 
+class OTsystem:
+    def __init__(self, data):
+        #TODO Check that data has desired properties
+        # _data and _keys may not be necessary
+        self._data = data
+        self._keys = [i.input for i in self._data]
+        self._map = {x: y for x, y in zip(self._keys, self._data)}
+    def __getitem__(self, key):
+        return self._map[key]
+    @classmethod
+    def fromOTW(cls, filepath):
+        pass
+
+
 foo = Tableaux([[0, 1, 2, 3, 4],
                 [5, 6, 7, 8, 9],
                 [10, 11, 12, 13, 14]])
 
-print(foo)
+print(OTsystem([foo]))
