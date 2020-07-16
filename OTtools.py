afe4d5d922a645f318ec1ee6936a63d854329478
Max Tarlov <mtarlov@ucsc.edu>
2020-07-16 08:08:02 -0700

    Renaming Files

--- OTtools.py
+++ OTtools.py
@@ -1,119 +1,121 @@
-# Python module for dealing with OT
-def warn(warning):
-    print(warning)
-    return True
-    #TODO perhapse made this mutable and add interactive capabilities
+# Restarting OTtools
+import unittest
 
-class OTobject:
-    """Metaclass for expected contents of a Tableau instance
-    
-    Ensures that contents of a Tableau instance support string casting
-
-    Attrabutes:
-        value (any): python representation of the object.
-        id (str): string representation of the object.
-    """
+class TableauObject:
+    _keyType = object  
 
     def __init__(self, value):
-        """Initializes new OTobject instance
-        
-        Parameter: value (any): representation of the object.
-            Must be castable to str
-        """
-        self.id = str(value)
         self.value = value
+        self.violations = None
+
+    def setViolations(self, violations):
+        if self.violations:
+            pass
+            #TODO: raise an exception
+        else:
+            for k, v in violations.items():
+                assert isinstance(k, self._keyType)
+                assert isinstance(v, int)
+            self.violations = violations
+
+    def __str__(self):
+        return str(self.value)
+
+class Candidate(TableauObject):
+    pass
+    # _keyType defined below
+
+class Constraint(TableauObject):
+    # _keyType defined below
+    def filter(self, candidates):
+        minimum = min([c.violations[self] for c in candidates])
+        result = []
+        for c, v in self.violations.items():
+            if v == minimum:
+                result.append(c)
+        return result
+
+Candidate._keyType = Constraint
+Constraint._keyType = Candidate
+
+class OTobject:
+    def __init__(self, input, constraints, candidates):
+        self.input = input
+        self.candidates = candidates
+        for c in candidates:
+            assert isinstance(c, Candidate)
+        self.constraints = constraints
+        for c in constraints:
+            assert isinstance(c, Constraint)
+        
+        if candidates[0].violations and not constraints[0].violations:
+            for con in constraints:
+                con.setViolations({can: can.violations[con] for can in candidates})
+        elif constraints[0].violations and not candidates[0].violations:
+            for can in candidates:
+                can.setViolations({con: con.violations[can] for con in constraints})
+
+    @classmethod
+    def fromMatrix(cls, matrix):
+        input = matrix[0][0]
+        constraints = [Constraint(con) for con in matrix[0][1:]]
+        candidates = [Candidate(row[0]) for row in matrix[1:]]
+        for can, row in zip(candidates, matrix[1:]):
+            can.setViolations({con: int(v) for con, v in zip(constraints, row[1:])})
+        return cls(input, constraints, candidates)
+
+    def getConstraintList(self):
+        return [str(c) for c in self.constraints]
+
+class LEG(OTobject):
+    @classmethod
+    def fromTableau(cls, tableau):
+        return cls(tableau.input, tableau.constraints, tableau.candidates)
     
-    def __str__(self):
-        return self.id
-    #TODO add __repr__
+    def evaluate(self):
+        passing = self.constraints[0].filter(self.candidates)
+        if len(self.constraints) > 1 and len(passing) > 1:
+            return LEG(self.input, self.constraints[1:], passing).evaluate()
+        else:
+            return passing
 
-class Candidate(OTobject):
-    """Extends OTobject
+class Tableau(OTobject):
+    def getOptima(self):
+        optima = []
+        for c in self.constraints:
+            passing = c.filter(self.candidates)
+            if len(self.constraints) > 1 and len(passing) > 1:
+                newCON = [x for x in self.constraints if x != c]
+                optima += Tableau(self.input, newCON, passing).getOptima()
+            else:
+                optima += passing
+        result = []
+        for c in optima:
+            if c not in result:
+                result.append(c)
+        return result
 
-    Nothing extra yet.
-    TODO: Add property self.optimal and 
-    """
-    def foo(self):
-        pass
-
-class Constraint(OTobject):
-    """Extends OTobject
-
-    Nothing extra yet.
-    """
-    def foo(self):
-        pass
-
-class Tableau:
-    """Datatype for an OT violation Tableau
-
-    Properties:
-        input (any): input to the tableau
-        violations (list): 2D array of violation counts
-        constraintSet (list): Constraint objects in the tableau
-        constraints (dict): constraint : violations for constraint
-        candidateSet (list): Candidate objects in the tableau
-        TODO:
-        candidates (dict): candidate : violations for candidate
-        _matrixColumns (list)
-        test _matrixRows
-    """
-
-    def __init__(self, matrix):
-        """Default Tableau constructor
-
-        Parameter: matrix (list): expected form:
-        [[input,     constraint1, ... constraintN],
-         [candiateA, violations,  ... violations],
-         :
-         :
-         [candidateN, violations, ... violations]
-        ]
-        """
-        self.input = matrix[0][0]
-        self.violations = [i[1:] for i in matrix[1:]]
-        self.constraintSet = [Constraint(i) for i in matrix[0][1:]]
-        self.candidateSet = [Candidate(i[0]) for i in matrix[1:]]
-        #this might need to be cast to a string:
-        self.constraints = {c: v for c, v in zip(self.constraintSet, self.violations)}
-        #self.candidates = {c: v for c, v in zip(self.candidateSet, )}
-        self._matrixRows = [[self.input] + [i for i in self.constraintSet]]
-        self._matrixRows += [[j] + v for j, v in zip(self.candidateSet, self.violations)]
-        #self._matrixColumns = 
-    def __str__(self):
-        result = [[str(i) for i in j] for j in self._matrixRows]
-        return str(result)
+    def getLEGs(self):
+        result = []
+        if len(self.constraints) > 1:
+            for c in self.constraints:
+                newCON = [x for x in self.constraints if x != c]
+                for o in Tableau(self.input, newCON, self.candidates).getLEGs():
+                    result.append(o)
+        return [LEG.fromTableau(t) for t in result]
 
 class OTsystem:
-    def __init__(self, data):
-        #TODO Check that data has desired properties
-        # _data and _keys may not be necessary
-        self._data = data
-        self._keys = [i.input for i in self._data]
-        self._map = {x: y for x, y in zip(self._keys, self._data)}
-    def __getitem__(self, key):
-        return self._map[key]
+    def getConstraintList(self):
+        return self.tableaux[0].getConstraintList()
+
+    def __init__(self, tableaux):
+        self.tableaux = tableaux
+        for t in self.tableaux:
+            #maybe this should be value instead of string
+            assert t.getConstraintList() == self.getConstraintList()
+
     @classmethod
     def fromOTW(cls, filepath):
-        """OTsystem constructor that accepts csv file in OTWorkplace layout
-
-        Parameter: filepath (str): path of the OTW file
-
-        Expected layout of file:
-            '', ...
-            '',       '', '',           'constraint1', 'constraint2', ...
-            'input1', 'candidateA', '', 'violations',  'violations',  ...
-            '',       'candidateB', '', 'violations',  'violations',  ...
-            :
-            :
-            'inputN', 'candidateA', '', 'violations',  'violations',  ...
-            :
-            :
-            newline
-        
-        TODO: add csv reading options, add support for optima
-        """
-
         import csv
         
         matricies = []
@@ -130,15 +132,42 @@ class OTsystem:
                     this_matrix = [[row[0]] + constraintSet]
                 this_matrix.append([row[1]] + row[3:])
 
-        tableaux = [Tableau(i) for i in matricies]
+            matricies.append(this_matrix)
+
+        tableaux = [Tableau.fromMatrix(i) for i in matricies]
         return cls(tableaux)
 
-def filter(candidates):
-    pass
+    #TODO re-evaluate this:
+    @staticmethod
+    def _writeCandidates(csv_writer, candidates, constraints):
+        for cand in candidates:
+            violations = [con.violations[cand] for con in constraints]
+            csv_writer.writerow(['', str(cand), ''] + violations)
 
-foo = Tableau([[0, 1, 2, 3, 4],
-                [5, 6, 7, 8, 9],
-                [10, 11, 12, 13, 14]])
+    def toOTW(self, filepath):
+        import csv
 
-bar = OTsystem.fromOTW('shortVT.csv')
-print(bar['[[workers] [helped]]'])
+        with open(filepath, 'w', newline='') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow([])
+            csv_writer.writerow(['', '', ''] + self.getConstraintList())
+            for t in self.tableaux:
+                violations0 = [con.violations[t.candidates[0]] for con in t.constraints]
+                csv_writer.writerow([str(t.input), str(t.candidates[0]), ''] + violations0)
+                self._writeCandidates(csv_writer, t.candidates[1:], t.constraints)
+
+
+if __name__ == '__main__':
+    system = OTsystem.fromOTW('testVT.csv')
+    assert len(system.tableaux) == 4
+    assert len(system.getConstraintList()) == 5
+    assert len(system.tableaux[0].candidates) == 2
+    assert len(system.tableaux[3].candidates) == 8
+    assert len(LEG.fromTableau(system.tableaux[0]).candidates) == 2
+    #print(system.tableaux[0].candidates[0])
+    #print(LEG.fromTableau(system.tableaux[3]).evaluate()[0])
+    print(system.tableaux[0].getLEGs())
+    system.toOTW('testExport.csv')
+    print('green')
+
+        
---
+++ oldOTtools.py
@@ -0,0 +1,144 @@
+# Python module for dealing with OT
+def warn(warning):
+    print(warning)
+    return True
+    #TODO perhapse made this mutable and add interactive capabilities
+
+class OTobject:
+    """Metaclass for expected contents of a Tableau instance
+    
+    Ensures that contents of a Tableau instance support string casting
+
+    Attrabutes:
+        value (any): python representation of the object.
+        id (str): string representation of the object.
+    """
+
+    def __init__(self, value):
+        """Initializes new OTobject instance
+        
+        Parameter: value (any): representation of the object.
+            Must be castable to str
+        """
+        self.id = str(value)
+        self.value = value
+    
+    def __str__(self):
+        return self.id
+    #TODO add __repr__
+
+class Candidate(OTobject):
+    """Extends OTobject
+
+    Nothing extra yet.
+    TODO: Add property self.optimal and 
+    """
+    def foo(self):
+        pass
+
+class Constraint(OTobject):
+    """Extends OTobject
+
+    Nothing extra yet.
+    """
+    def foo(self):
+        pass
+
+class Tableau:
+    """Datatype for an OT violation Tableau
+
+    Properties:
+        input (any): input to the tableau
+        violations (list): 2D array of violation counts
+        constraintSet (list): Constraint objects in the tableau
+        constraints (dict): constraint : violations for constraint
+        candidateSet (list): Candidate objects in the tableau
+        TODO:
+        candidates (dict): candidate : violations for candidate
+        _matrixColumns (list)
+        test _matrixRows
+    """
+
+    def __init__(self, matrix):
+        """Default Tableau constructor
+
+        Parameter: matrix (list): expected form:
+        [[input,     constraint1, ... constraintN],
+         [candiateA, violations,  ... violations],
+         :
+         :
+         [candidateN, violations, ... violations]
+        ]
+        """
+        self.input = matrix[0][0]
+        self.violations = [i[1:] for i in matrix[1:]]
+        self.constraintSet = [Constraint(i) for i in matrix[0][1:]]
+        self.candidateSet = [Candidate(i[0]) for i in matrix[1:]]
+        #this might need to be cast to a string:
+        self.constraints = {c: v for c, v in zip(self.constraintSet, self.violations)}
+        #self.candidates = {c: v for c, v in zip(self.candidateSet, )}
+        self._matrixRows = [[self.input] + [i for i in self.constraintSet]]
+        self._matrixRows += [[j] + v for j, v in zip(self.candidateSet, self.violations)]
+        #self._matrixColumns = 
+    def __str__(self):
+        result = [[str(i) for i in j] for j in self._matrixRows]
+        return str(result)
+
+class OTsystem:
+    def __init__(self, data):
+        #TODO Check that data has desired properties
+        # _data and _keys may not be necessary
+        self._data = data
+        self._keys = [i.input for i in self._data]
+        self._map = {x: y for x, y in zip(self._keys, self._data)}
+    def __getitem__(self, key):
+        return self._map[key]
+    @classmethod
+    def fromOTW(cls, filepath):
+        """OTsystem constructor that accepts csv file in OTWorkplace layout
+
+        Parameter: filepath (str): path of the OTW file
+
+        Expected layout of file:
+            '', ...
+            '',       '', '',           'constraint1', 'constraint2', ...
+            'input1', 'candidateA', '', 'violations',  'violations',  ...
+            '',       'candidateB', '', 'violations',  'violations',  ...
+            :
+            :
+            'inputN', 'candidateA', '', 'violations',  'violations',  ...
+            :
+            :
+            newline
+        
+        TODO: add csv reading options, add support for optima
+        """
+
+        import csv
+        
+        matricies = []
+
+        with open(filepath) as csv_file:
+            csv_reader = list(csv.reader(csv_file))
+            constraintSet = csv_reader[1][3:]
+            this_matrix = [[csv_reader[2][0]] + constraintSet]
+            this_matrix.append([csv_reader[2][1]] + csv_reader[2][3:])
+
+            for row in csv_reader[3:]:
+                if len(row[0]):
+                    matricies.append(this_matrix)
+                    this_matrix = [[row[0]] + constraintSet]
+                this_matrix.append([row[1]] + row[3:])
+
+        tableaux = [Tableau(i) for i in matricies]
+        return cls(tableaux)
+
+def filter(candidates):
+    pass
+
+foo = Tableau([[0, 1, 2, 3, 4],
+                [5, 6, 7, 8, 9],
+                [10, 11, 12, 13, 14]])
+
+bar = OTsystem.fromOTW('shortVT.csv')
+print(bar['[[workers] [helped]]'])
